4c760adc0633ddc2ad5889176f905c44d570b01a
SARAVANAKANTH16 <128817809+SARAVANAKANTH16@users.noreply.github.com>
2023-10-25 19:30:43 +0530

    Add files via upload

---
+++ model/bookModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const bookSchema = new mongoose.Schema({
+    title:String,
+    author:String
+});
+
+const bookModel = mongoose.model('book',bookSchema);
+module.exports = bookModel;
